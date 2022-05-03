44c117b39c676bea2d441c3464c10cf199c2ded9
alex90o <alex10o@hotmail.com>
2022-05-03 09:05:13 -0300

    se creo la funcion que relaciona las tablas

--- app/Permisos/Models/Role.php
+++ app/Permisos/Models/Role.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    //es: desde aca comienza
+    protected $fillable = [
+        'nombre', 'slug', 'descripcion', 'full-access'
+    ];
+
+    public function users(){
+        return $this->belongsToMany('App\User')->withTimestamps();
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+       //es: desde aca comienza
+
+    public function roles(){
+        return $this->belongsToMany('App\Permisos\Models\Role')->withTimestamps();
+    }
 }
