bf27d82f2364628be6dbc6976523626a7fb4c7ff
alex90o <alex10o@hotmail.com>
2022-05-06 09:43:01 -0300

    se actualizo create y uso la variable old en los campos

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -18,26 +18,41 @@
                         <div class="container">
                             <h3>Requiere datos</h3>
                             <div class="form-group">
-                                <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre">
+                                <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre"
+                                value="{{old('nombre')}}">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id="slug" name="slug" placeholder="Slug">
+                                <input type="text" class="form-control" id="slug" name="slug" placeholder="Slug"
+                                value="{{old('slug')}}">
                             </div>
 
                             <div class="form-group">
                                 <textarea class="form-control" placeholder="Descripción" id="descripcion"
-                                    name="descripcion" rows="3"></textarea>
+                                    name="descripcion" rows="3" >
+                                    {{old('descripcion')}}
+                                </textarea>
                             </div>
                             <hr>
                             <h3>Acceso Completo</h3>
                             <div class="custom-control custom-radio custom-control-inline">
                                 <input type="radio" id="fullaccessyes" name="full-access"
-                                    class="custom-control-input" value="yes">
+                                    class="custom-control-input" value="yes"
+                                    @if (old ('full-access')=="yes")
+                                        checked
+                                    @endif
+                                    >
                                 <label class="custom-control-label" for="fullaccessyes">Si</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
                                 <input type="radio" id="fullaccessno" name="full-access"
-                                    class="custom-control-input" value="no" checked>
+                                    class="custom-control-input" value="no"
+                                    @if (old ('full-access')=="no")
+                                        checked
+                                    @endif
+                                    @if (old ('full-access')===null)
+                                        checked
+                                    @endif
+                                    >
                                 <label class="custom-control-label" for="fullaccessno">No</label>
                             </div>
                             <hr>
@@ -47,7 +62,11 @@
                             <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" 
                                 id="permiso_{{$permiso->id}}"
-                                value="{{ $permiso->id}}" name="permiso[]">
+                                value="{{ $permiso->id}}" name="permiso[]"
+                                @if(is_array (old('permiso')) && in_array("$permiso->id", old ('permiso')))
+                                checked
+                                @endif>
+                                
                                 <label class="custom-control-label" for="permiso_{{$permiso->id}}">
                                     {{ $permiso->id}}
                                     -
@@ -62,6 +81,8 @@
                         </div>
 
                     </form>
+
+ 
 
 
 
